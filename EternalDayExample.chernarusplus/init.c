97b33ec98057452811505eb52e84e637bd1ee63f
DjCtavia <flan-etienne520@live.com>
2018-11-12 08:37:30 +0100

    Server files example

---
+++ EternalDayExample.chernarusplus/init.c
@@ -0,0 +1,94 @@
+
+void main()
+{
+
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
+
+	Weather weather = g_Game.GetWeather();
+
+	weather.GetOvercast().SetLimits( 0.0 , 1.0 );
+	weather.GetRain().SetLimits( 0.0 , 1.0 );
+	weather.GetFog().SetLimits( 0.0 , 0.25 );
+
+	weather.GetOvercast().SetForecastChangeLimits( 0.0, 0.2 );
+	weather.GetRain().SetForecastChangeLimits( 0.0, 0.1 );
+	weather.GetFog().SetForecastChangeLimits( 0.15, 0.45 );
+
+	weather.GetOvercast().SetForecastTimeLimits( 1800 , 1800 );
+	weather.GetRain().SetForecastTimeLimits( 600 , 600 );
+	weather.GetFog().SetForecastTimeLimits( 1800 , 1800 );
+
+	weather.GetOvercast().Set( Math.RandomFloatInclusive(0.0, 0.3), 0, 0);
+	weather.GetRain().Set( Math.RandomFloatInclusive(0.0, 0.2), 0, 0);
+	weather.GetFog().Set( Math.RandomFloatInclusive(0.0, 0.1), 0, 0);
+	
+	weather.SetWindMaximumSpeed(30);
+	weather.SetWindFunctionParams(0.1, 1.0, 50);
+}
+
+class CustomMission: MissionServer
+{	
+
+	// This is the member
+	ref EternalDay		m_EternalDay;
+	
+	// This is the Constructor, by default, CustomMission don't have one. It's called when class CustoMission is created.
+	void CustomMission()
+	{
+		// We create the reference of the class EternalDay, and linked it to the class member named: 'm_EternalDay'
+		m_EternalDay = new ref EternalDay();
+		
+/* 		We can set a new specific time slot if we want too
+
+		m_EternalDay.setDayTime(6.5, 12.75);	//	This set time slot from 6:30AM to 12:45PM
+		
+		m_EternalDay.setRandomTime();			//	Set a random time between time slot [WIP]
+		m_EternalDay.setRandomDate();			//	Set a random date [WIP]
+*/
+	}
+
+	void SetRandomHealth(EntityAI itemEnt)
+	{
+		int rndHlt = Math.RandomInt(40,100);
+		itemEnt.SetHealth("","",rndHlt);
+	}
+
+	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+	{
+		Entity playerEnt;
+		playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");//Creates random player
+		Class.CastTo(m_player, playerEnt);
+		
+		GetGame().SelectPlayer(identity, m_player);
+		
+		return m_player;
+	}
+	
+	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	{
+/*
+		player.RemoveAllItems();
+
+		EntityAI item = player.GetInventory().CreateInInventory(topsArray.GetRandomElement());
+		EntityAI item2 = player.GetInventory().CreateInInventory(pantsArray.GetRandomElement());
+		EntityAI item3 = player.GetInventory().CreateInInventory(shoesArray.GetRandomElement());
+*/
+		EntityAI itemEnt;
+		ItemBase itemBs;
+		
+		itemEnt = player.GetInventory().CreateInInventory("Rag");
+		itemBs = ItemBase.Cast(itemEnt);
+		itemBs.SetQuantity(4);
+		SetRandomHealth(itemEnt);
+
+		itemEnt = player.GetInventory().CreateInInventory("RoadFlare");
+		itemBs = ItemBase.Cast(itemEnt);
+	}
+};
+  
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}
